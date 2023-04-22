02f1b58d5f36f6be1c2bdfeb5fd9c768c17034e9
Bohdan <nadgobvoravohus@gmail.com>
2023-04-23 01:25:55 +0200

    added key controls

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -1,65 +1,74 @@
-import java.awt.*;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener {
 
-    private int gridSize = 20; //set grid size n^2
-    private Timer timer;
-    private Snake snake;
-    public GamePanel(){
+    private final int gridSize = 20; //set grid size n^2
+    private final Timer timer;
+    private final Snake snake;
+
+    public GamePanel() {
         setPreferredSize(new Dimension(gridSize * 20, gridSize * 20));
         setBackground(Color.BLACK);
         snake = new Snake();
+        setFocusable(true);
+        addKeyListener(this);
 
 
         timer = new Timer(300, new ActionListener() {
             @Override
-        public void actionPerformed(ActionEvent e) {
-            tick(snake);
-        }
-    });
+            public void actionPerformed(ActionEvent e) {
+                tick(snake);
+            }
+        });
         timer.start();
     }
 
 
-    public void tick(Snake snake){
-        snake.move(1,0);
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.VK_UP:
+                snake.move(0, -1);
+                break;
+            case KeyEvent.VK_DOWN:
+                snake.move(0, 1);
+                break;
+            case KeyEvent.VK_LEFT:
+                snake.move(-1, 0);
+                break;
+            case KeyEvent.VK_RIGHT:
+                snake.move(1, 0);
+                break;
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent keyEvent) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+    public void tick(Snake snake) {
+        snake.move(0,0);
         repaint();
     }
 
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Snake snakeInstance = snake;
 
-
-        /* debug code
-        // in matrices and graphs, the vertical axis (y-axis) is from top to bottom and the horizontal axis (x-axis) is from left to right.
-        snake snake = new snake();
-        snake.move(0, 1); //  d
-        snake.grow();
-        snake.move(1, 0); // Движение вправо
-        snake.grow();
-        snake.move(0, -1); // Движение вверх
-        snake.grow();
-        snake.move(-1, 0); // Движение влево
-        snake.grow();
-
-
-        List<Point> segments = snake.getSegments();
-        for (Point p : segments) {
-            System.out.println("head: (" + p.x + ", " + p.y + ")");
-        }
-        */
-
-       for (int i = 0; i < gridSize; i++) {
-            for (int j = 0; j < gridSize; j++) {
-                g.drawRect(i * 20, j * 20, 20, 20);  // i and j we will use as coordinates for the grid
-            }
-        }
 
         for (Point segment : snakeInstance.getSegments()) {
             g.setColor(Color.WHITE);
@@ -73,6 +82,7 @@ public class GamePanel extends JPanel {
     public Dimension getPreferredSize() {
         return new Dimension(gridSize * 20, gridSize * 20);
     }
+
     @Override
     public Dimension getMinimumSize() {
         return getPreferredSize();
